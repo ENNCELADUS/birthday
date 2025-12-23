21f71f8c1ba20544f9069f569589590806280457
Richard Wang <your-email@example.com>
2025-12-24 04:39:52 +0800

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'export',
+    basePath: '/birthday',
+    images: {
+        unoptimized: true,
+    },
     transpilePackages: ['three', '@react-three/drei', '@react-three/fiber', '@react-three/postprocessing', 'maath'],
 };
 
